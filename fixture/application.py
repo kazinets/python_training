b898721cd5edda44fe3d01bea541bcee69f72720
ekaterinakazinets <k.kazinets@gmail.com>
2019-07-23 20:53:18 +0300

    Выделен класс с работы с группами

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.firefox.webdriver import WebDriver
+from fixture.group import GroupHelper
+
 
 
 class Application:
@@ -7,6 +9,7 @@ class Application:
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(30)
+        self.group=GroupHelper (self)
 
 
     def add_new_contact(self, contact):
@@ -52,26 +55,6 @@ class Application:
         wd = self.wd
         wd.get("http://localhost/addressbookv4/index.php")
         self.tap_on_home_menu_item()
-
-    def create_new_group(self, group):
-            wd = self.wd
-            wd.find_element_by_name("new").click()
-            wd.find_element_by_name("group_name").click()
-            wd.find_element_by_name("group_name").clear()
-            wd.find_element_by_name("group_name").send_keys(group.name)
-            wd.find_element_by_name("group_header").click()
-            wd.find_element_by_name("group_header").clear()
-            wd.find_element_by_name("group_header").send_keys(group.header)
-            wd.find_element_by_name("group_footer").click()
-            wd.find_element_by_name("group_footer").clear()
-            wd.find_element_by_name("group_footer").send_keys(group.footer)
-            # click on Submit button
-            wd.find_element_by_name("submit").click()
-
-    def open_group_page(self):
-            wd = self.wd
-            wd.find_element_by_link_text("groups").click()
-
 
 
     def destroy(self):
--- test/test_create_new_group.py
+++ test/test_create_new_group.py
@@ -13,14 +13,14 @@ def test_create_empty_group(app):
 
     app.open_home_page()
     # Open Group
-    app.open_group_page()
-    app.create_new_group(Group(name="", header="", footer=""))
-    app.open_group_page()
+    app.group.open_group_page()
+    app.group.create(Group(name="", header="", footer=""))
+    app.group.open_group_page()
 
     
 def test_create_group(app):
     app.open_home_page()
     # Open Group
-    app.open_group_page()
-    app.create_new_group(Group(name="First Group", header="logo", footer="comment 1"))
-    app.open_group_page()
+    app.group.open_group_page()
+    app.group.create(Group(name="First Group", header="logo", footer="comment 1"))
+    app.group.open_group_page()
