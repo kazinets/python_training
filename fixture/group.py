faed9fbeb990736573f0149a861ca1010eb889cc
ekaterinakazinets <k.kazinets@gmail.com>
2019-07-25 19:56:34 +0300

    Add test_edit_first_group test

--- fixture/group.py
+++ fixture/group.py
@@ -30,6 +30,25 @@ class GroupHelper:
         # delete first group
         wd.find_element_by_name("delete").click()
 
+    def edit_first_group(self,group):
+            wd = self.app.wd
+            self.open_group_page()
+            # select first group
+            wd.find_element_by_name("selected[]").click()
+            # delete first group
+            wd.find_element_by_name("edit").click()
+            #enter new value
+            wd.find_element_by_name("group_name").send_keys(group.name)
+            wd.find_element_by_name("group_header").click()
+            wd.find_element_by_name("group_header").clear()
+            wd.find_element_by_name("group_header").send_keys(group.header)
+            wd.find_element_by_name("group_footer").click()
+            wd.find_element_by_name("group_footer").clear()
+            wd.find_element_by_name("group_footer").send_keys(group.footer)
+            # click on Update button
+            wd.find_element_by_name("update").click()
+
+
 
 
 
