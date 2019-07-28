f31071dd88d44287cca5b3d9ab784261646beefc
ekaterinakazinets <k.kazinets@gmail.com>
2019-07-28 15:49:46 +0300

    Add session fixture

--- fixture/application.py
+++ fixture/application.py
@@ -2,6 +2,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
+from fixture.session import SessionHelper
 
 
 
@@ -10,9 +11,9 @@ class Application:
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(30)
+        self.session=SessionHelper(self)
         self.group=GroupHelper (self)
         self.contact=ContactHelper(self)
-
 
 
     def tap_on_home_menu_item(self):
@@ -22,7 +23,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbookv4/index.php")
-        self.tap_on_home_menu_item()
+        #self.tap_on_home_menu_item()
 
     def return_to_home_page(self):
         wd=self.wd
--- test/test_create_new_group.py
+++ test/test_create_new_group.py
@@ -2,16 +2,21 @@ from model.group import Group
 
 def test_create_empty_group(app):
 
-    app.open_home_page()
+    app.login("admin","secret")
+
     # Open Group
     app.group.open_group_page()
     app.group.create(Group(name="", header="", footer=""))
     app.group.open_group_page()
+    app.logout()
 
     
 def test_create_group(app):
-    app.open_home_page()
+    app.session.login("admin", "secret")
+
+    #app.open_home_page()
     # Open Group
     app.group.open_group_page()
     app.group.create(Group(name="First Group", header="logo", footer="comment 1"))
     app.group.open_group_page()
+    app.session.logout()
