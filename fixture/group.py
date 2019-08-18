65390bbff3cbb014c40164fa425ba911a49e9a0a
ekaterinakazinets <k.kazinets@gmail.com>
2019-08-18 12:45:09 +0300

    update create and edit contacts

--- fixture/group.py
+++ fixture/group.py
@@ -6,15 +6,8 @@ class GroupHelper:
     def create(self, group):
             wd = self.app.wd
             wd.find_element_by_name("new").click()
-            wd.find_element_by_name("group_name").click()
-            wd.find_element_by_name("group_name").clear()
-            wd.find_element_by_name("group_name").send_keys(group.name)
-            wd.find_element_by_name("group_header").click()
-            wd.find_element_by_name("group_header").clear()
-            wd.find_element_by_name("group_header").send_keys(group.header)
-            wd.find_element_by_name("group_footer").click()
-            wd.find_element_by_name("group_footer").clear()
-            wd.find_element_by_name("group_footer").send_keys(group.footer)
+            self.fill_group_form(group)
+
             # click on Submit button
             wd.find_element_by_name("submit").click()
 
@@ -37,17 +30,22 @@ class GroupHelper:
             wd.find_element_by_name("selected[]").click()
             # delete first group
             wd.find_element_by_name("edit").click()
-            #enter new value
-            wd.find_element_by_name("group_name").send_keys(group.name)
-            wd.find_element_by_name("group_header").click()
-            wd.find_element_by_name("group_header").clear()
-            wd.find_element_by_name("group_header").send_keys(group.header)
-            wd.find_element_by_name("group_footer").click()
-            wd.find_element_by_name("group_footer").clear()
-            wd.find_element_by_name("group_footer").send_keys(group.footer)
+            self.fill_group_form(group)
+
             # click on Update button
             wd.find_element_by_name("update").click()
 
+    def fill_group_form(self, group):
+        wd=self.app.wd
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
 
 
 
--- test/test_edit_first_group.py
+++ test/test_edit_first_group.py
@@ -2,15 +2,10 @@ from model.group import Group
 
 def test_edit_first_group(app):
 
-    #Add creation new group because of empty group list
+
     #app.open_home_page()
     app.session.login("admin", "secret")
     # Open Group
-    app.group.open_group_page()
-    app.group.create(Group(name="First Group", header="logo", footer="comment 1"))
-    app.group.open_group_page()
-
-    #edit group
 
     app.open_home_page()
     # Open Group
