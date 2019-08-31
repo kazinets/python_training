4cf4da61d6b4277509e7fcf3e5a78de475cafa7e
ekaterinakazinets <k.kazinets@gmail.com>
2019-08-31 18:16:04 +0300

    Сортировка групп и сравнение

--- test/test_create_new_group.py
+++ test/test_create_new_group.py
@@ -1,25 +1,30 @@
 from model.group import Group
+from sys import maxsize
 
 def test_create_empty_group(app):
 
 
     app.group.open_group_page()
     old_groups = app.group.get_group_list()
+    group = Group(name="", header="", footer="")
     app.group.create(Group(name="", header="", footer=""))
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max)==sorted(new_groups,key=Group.id_or_max)
 
-
-    
 def test_create_group(app):
 
 
     app.group.open_group_page()
 
     old_groups=app.group.get_group_list()
-
-    app.group.create(Group(name="First Group", header="logo", footer="comment 1"))
+    group=Group(name="First Group", header="logo", footer="comment 1")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups)+1 == len(new_groups)
-    #app.return_to_home_page()
+    old_groups.append(group)
+
+    assert sorted(old_groups, key=Group.id_or_max) ==sorted(new_groups, key=Group.id_or_max)
+
 
--- test/test_edit_first_group.py
+++ test/test_edit_first_group.py
@@ -2,20 +2,25 @@ from model.group import Group
 
 def test_edit_first_group(app):
 
-
     app.open_home_page()
     old_groups = app.group.get_group_list()
     app.group.open_group_page()
-
+    group = Group(name="First")
+    group.id=old_groups[0].id
     if app.group.count()==0:
-        app.group.create(Group(name="First"))
-    app.group.edit_first_group(Group(name="New Group name", header="new logo", footer="new comment 2"))
+        app.group.create(group)
+
+    app.group.edit_first_group(group)
+    old_groups[0]=group
     new_groups = app.group.get_group_list()
     assert len(old_groups)  == len(new_groups)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
-def test_modify_group_header(app):
+
+
+'''def test_modify_group_header(app):
 
     app.open_home_page()
     old_groups = app.group.get_group_list()
@@ -26,4 +31,4 @@ def test_modify_group_header(app):
     app.group.edit_first_group(Group(header="HEADER!!!"))
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-
+'''
