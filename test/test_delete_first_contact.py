84a56e6069fa775ca02101011d6051865fbcfb6d
ekaterinakazinets <k.kazinets@gmail.com>
2019-09-01 17:01:32 +0300

    Добавлены проверки в Удаление контакта

--- test/test_delete_first_contact.py
+++ test/test_delete_first_contact.py
@@ -7,5 +7,11 @@ def test_delete_first_contact(app):
         app.contact.add_new_contact(Contact("first_name", "KazinetsLastYYYYY", "Address", "Homw1", "+375295107204", "work", "email1@tut.by",
                              "email2@tut.by", "19", "December", "1985", "address2", "home2"))
     app.open_home_page()
+    old_contact=app.contact.get_contact_list()
     app.contact.delete_first_contact()
     app.tap_on_home_menu_item()
+    app.open_home_page()
+    new_contact = app.contact.get_contact_list()
+#    assert len(old_contact) - 1 == len(new_contact)
+    #old_contact[0:1] = []
+    assert old_contact == new_contact
